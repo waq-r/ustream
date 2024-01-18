72a6e9b51570854b020e3bc40b1990c56ed05a6b
waqar3 <ahmed@waqar.org>
2024-01-18 10:47:36 +0000

    epic stack template setup, first commit

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const http = require('http');
+import 'dotenv/config'
+import * as fs from 'fs'
+import { installGlobals } from '@remix-run/node'
+import chalk from 'chalk'
+import closeWithGrace from 'close-with-grace'
+import sourceMapSupport from 'source-map-support'
 
-const hostname = 'localhost';
-const port = 3000;
+sourceMapSupport.install({
+	retrieveSourceMap: function (source) {
+		// get source file without the `file://` prefix or `?t=...` suffix
+		const match = source.match(/^file:\/\/(.*)\?t=[.\d]+$/)
+		if (match) {
+			return {
+				url: source,
+				map: fs.readFileSync(`${match[1]}.map`, 'utf8'),
+			}
+		}
+		return null
+	},
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('GitHub Actions!\n');
-});
+installGlobals()
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+closeWithGrace(async ({ err }) => {
+	if (err) {
+		console.error(chalk.red(err))
+		console.error(chalk.red(err.stack))
+		process.exit(1)
+	}
+})
 
+if (process.env.MOCKS === 'true') {
+	await import('./tests/mocks/index.ts')
+}
+
+if (process.env.NODE_ENV === 'production') {
+	await import('./server-build/index.js')
+} else {
+	await import('./server/index.ts')
+}
